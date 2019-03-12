edbbc11ea7e49e52f17622b3312576dadfe9539f
Luke Gao <gaoyuan0508@outlook.com>
2019-03-12 21:38:46 +0800

    添加注释

--- src/app/core/service/electron/electron.service.ts
+++ src/app/core/service/electron/electron.service.ts
@@ -1,7 +1,4 @@
 import { Injectable, NgZone } from '@angular/core';
-
-// If you import a module but never use any of the imported values other than as TypeScript types,
-// the resulting javascript file will look as if you never imported the module at all.
 import { ipcRenderer, webFrame, remote } from 'electron';
 import * as childProcess from 'child_process';
 import * as fs from 'fs';
@@ -15,7 +12,6 @@ export class ElectronService {
   fs: typeof fs;
   // 当前窗口是否是最大化
   isMaximized = false;
-
   constructor( private _ngzone: NgZone) {
     // Conditional imports
     if (this.isElectron()) {
@@ -27,24 +23,28 @@ export class ElectronService {
       this.fs = window.require('fs');
     }
   }
-
+  // 检测当前是否是Electron运行模式
   isElectron = () => {
     return window && window.process && window.process.type;
   }
+  // 窗口的最大化、最小化和关闭调整
   adjustWin(type) {
-    // this.isMaximized = !this.isMaximized;
     if (this.isElectron()) {
       const win = this.remote.getCurrentWindow();
       switch (type) {
+        // 最大化
         case 'MAX':
           win.maximize();
           break;
+        // 最小化
         case 'MIN':
           win.minimize();
           break;
+        // 最大化还原
         case 'UNMAX':
           win.unmaximize();
           break;
+        // 窗口关闭
         case 'CLOSE':
           win.close();
           break;
@@ -55,18 +55,18 @@ export class ElectronService {
   }
   listenWin() {
     const win = this.remote.getCurrentWindow();
+    // 监听当窗体最大化的时候执行
     win.on('maximize', () => {
+      // 对于Electron来说，状态的变更在Angular的自动变更检测之外，因此需要被动进行变更检测
       this._ngzone.run(() => {
         this.isMaximized = this.remote.getCurrentWindow().isMaximized();
       });
-
-      console.log('变大了');
     });
+    // 监听当窗口取消最大化的时候执行
     win.on('unmaximize', () => {
       this._ngzone.run(() => {
         this.isMaximized = this.remote.getCurrentWindow().isMaximized();
       });
-      console.log('变小了');
     });
   }
 }
