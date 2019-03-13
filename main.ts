dc17f4d87e57cc981cbb0a34405fc516e1227491
Luke <gaoyuan0508@outlook.com>
2019-03-14 00:25:40 +0800

    窗口拖动

--- main.ts
+++ main.ts
@@ -35,12 +35,13 @@ function createWindow() {
     require('electron-reload')(__dirname, {
       electron: require(`${__dirname}/node_modules/electron`)
     });
-    win.loadURL('http://localhost:4200');
+    win.loadURL('http://localhost:4200/#/login');
   } else {
     win.loadURL(url.format({
       pathname: path.join(__dirname, 'dist/index.html'),
       protocol: 'file:',
-      slashes: true
+      slashes: true,
+      hash: '/login'
     }));
   }
   // win.once('ready-to-show', () => {
