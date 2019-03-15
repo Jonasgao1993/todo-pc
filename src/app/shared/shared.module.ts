e7f13edb64795bc6ca0443cd6fd4d8f8e9f2912a
Luke Gao <gaoyuan0508@outlook.com>
2019-03-15 17:56:23 +0800

    sidebar icon样式调整

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,12 +7,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { WeatherComponent } from './widgets/weather/weather.component';
 import { FireworkComponent } from './widgets/firework/firework.component';
 import { CountUpModule } from 'countup.js-angular2';
+import { MomentModule } from 'ngx-moment';
+import { NgxFullCalendarModule } from 'ngx-fullcalendar';
+import { QuillModule } from 'ngx-quill';
 
 @NgModule({
   imports: [
-    CommonModule, NgZorroAntdModule, HttpClientModule,CountUpModule
+    CommonModule, NgZorroAntdModule, HttpClientModule, CountUpModule, MomentModule,
+    NgxFullCalendarModule, QuillModule
   ],
   declarations: [WebviewDirective, WeatherComponent, FireworkComponent],
-  exports: [NgZorroAntdModule, WeatherComponent, FireworkComponent]
+  exports: [NgZorroAntdModule, MomentModule, QuillModule,
+    NgxFullCalendarModule, WeatherComponent, FireworkComponent]
 })
 export class SharedModule { }
